49cc830d3b45751079555c904ae23fec2986ae18
Matt Hintzke <matt.hintzke@skykick.com>
2016-11-29 23:10:07 -0800

    Added comments

--- SkyKick.Kernel/KernelBuilder.cs
+++ SkyKick.Kernel/KernelBuilder.cs
@@ -7,8 +7,17 @@ using System.Threading.Tasks;
 
 namespace SkyKick.Kernel
 {
+    /// <summary>
+    /// KernelBuilder allows use to define what gets loaded into a kernel using the BuildKernel
+    /// override, however, each CreateKernel() call will generate a new instance of the kernel
+    /// allowing seperation of kernels if needed
+    /// </summary>
     public abstract class KernelBuilder : IKernelBuilder
     {
+        /// <summary>
+        /// Create a new instance of the kernel defined in BuildKernel and returns it
+        /// </summary>
+        /// <returns></returns>
         public IKernel CreateKernel()
         {
             // Build kernel using override
@@ -17,6 +26,11 @@ namespace SkyKick.Kernel
             return kernel;
         }
 
+        /// <summary>
+        /// Create a new instance of the kernel defined in BuildKernel, applies custom bindings and returns it
+        /// </summary>
+        /// <param name="Binder"></param>
+        /// <returns></returns>
         public IKernel CreateKernel(Func<IKernel, IKernel> Binder)
         {
             // Build kernel using override
@@ -26,6 +40,10 @@ namespace SkyKick.Kernel
             return Binder.Invoke(kernel);
         }
 
+        /// <summary>
+        /// Abstract method used to define Kernel
+        /// </summary>
+        /// <returns></returns>
         public abstract IKernel BuildKernel();
     }
 }
